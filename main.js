520efa98fa1c7ee43082c1b45d594bdf034a6e9f
tttom3669 <tttom3669@gmail.com>
2024-09-29 14:37:02 +0800

    update: 安裝 swiper

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+// import styles bundle
+import 'swiper/css/bundle';
+
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
-
-console.log("Hello world!");
