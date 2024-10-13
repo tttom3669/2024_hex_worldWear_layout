48681f84dee9add15622c089e0a9f7f0f148c026
tttom3669 <tttom3669@gmail.com>
2024-10-13 16:36:19 +0800

    update: header 登出/登入樣式切換

--- assets/js/shared.js
+++ assets/js/shared.js
@@ -1,28 +1,138 @@
-mainMenuHandler();
+import { gsap } from 'gsap';
+
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+(function init() {
+  mainMenuHandler();
+  headerGsap();
+})();
+
+/**
+ * 主要選單管理
+ */
 function mainMenuHandler() {
   const menuLinkGroup = document.querySelectorAll(`.dropdown > .l-menu__link `);
+  const collapseBtn = document.querySelector(`.l-menu__collapse-btn`);
+  const collapseContent = document.querySelector(`.l-menu__collapse`);
+  const searchTrigger = document.querySelector(`.header__searchIcon`);
+  const searchBar = document.querySelector(`.header__searchBar-container`);
+
+  // 關閉下拉選單內容
+  const closeContent = (target = 'all') => {
+    menuLinkGroup.forEach((link) => {
+      const href = link.attributes.href.value;
+      const dropdown = document.querySelector(`${href}`);
+      if (target === 'all' || target !== dropdown) {
+        link.classList.remove('active');
+        dropdown.classList.remove('show');
+      }
+    });
+
+    if (target === 'all' || target !== searchBar) {
+      searchBar.classList.remove('show');
+      searchTrigger.classList.remove('active');
+    }
+    if (target === 'all' || target !== collapseContent) {
+      collapseContent.classList.remove('show');
+      if (!collapseBtn.classList.contains('l-menu__collapse-btn--login')) {
+        const svgList = collapseBtn.children[0];
+        const svgClose = collapseBtn.children[1];
+        svgList.classList.toggle('d-none');
+        svgClose.classList.toggle('d-none');
+      }
+    }
+  };
+
+  collapseBtn.addEventListener('click', (e) => {
+    const content = document.querySelector(`.l-menu__collapse`);
+    const isCollapse = !content.classList.contains('show');
+
+    if (!collapseBtn.classList.contains('l-menu__collapse-btn--login')) {
+      const svgList = collapseBtn.children[0];
+      const svgClose = collapseBtn.children[1];
+      svgList.classList.toggle('d-none');
+      svgClose.classList.toggle('d-none');
+    }
+
+    if (isCollapse) {
+      closeContent(content);
+      content.classList.add('show');
+    } else {
+      closeContent();
+    }
+    // 防止點擊選單本身時關閉選單
+    e.stopPropagation();
+  });
+
+  searchTrigger.addEventListener('click', (e) => {
+    const isCollapse = !searchBar.classList.contains('show');
+
+    if (isCollapse) {
+      closeContent(searchBar);
+      searchBar.classList.add('show');
+      searchTrigger.classList.add('active');
+    } else {
+      closeContent();
+    }
+    e.stopPropagation();
+  });
+  searchBar.addEventListener('click', (e) => {
+    e.stopPropagation();
+  });
+
   menuLinkGroup.forEach((link) => {
     const href = link.attributes.href.value;
     const target = document.querySelector(`${href}`);
 
-    link.addEventListener('click', () => {
-      link.classList.toggle('active');
-      target.classList.toggle('show');
+    link.addEventListener('click', (e) => {
+      const isCollapse = !target.classList.contains('show');
+      if (isCollapse) {
+        closeContent(target);
+        link.classList.add('active');
+        target.classList.add('show');
+      } else {
+        closeContent();
+      }
+      // 防止點擊選單本身時關閉選單
+      e.stopPropagation();
     });
+  });
 
-    window.addEventListener('click', (e) => {
-      console.dir(e.target);
-      if (
-        e.target === link ||
-        e.target === target ||
-        e.target.offsetParent === target
-      ) {
-        return;
-      }
-      if (target.classList.contains('show')) {
-        target.classList.remove('show');
-        link.classList.remove('active');
-      }
+  // 點擊其他地方關閉所有選單
+  document.addEventListener('click', () => {
+    closeContent();
+    searchBar.classList.remove('show');
+  });
+
+  document.querySelectorAll('.l-menu__dropdown').forEach((dropdown) => {
+    dropdown.addEventListener('click', (e) => {
+      // 防止點擊選單本身時關閉選單
+      e.stopPropagation();
     });
+  });
+  collapseContent.addEventListener('click', (e) => {
+    // 防止點擊選單本身時關閉選單
+    e.stopPropagation();
+  });
+}
+
+function headerGsap() {
+  const header = document.querySelector('.header--dark');
+  gsap.registerPlugin(ScrollTrigger);
+
+  ScrollTrigger.create({
+    trigger: '.site-index__banner',
+    start: 'bottom top', // 當 banner 底部與 viewport 頂端對齊時觸發
+    end: 'bottom top', // 同樣的起點與終點
+    onEnter: () => {
+      // 向下滾動到達時添加 class
+      header.classList.remove('header--dark');
+      header.classList.add('header--light');
+    },
+    onLeaveBack: () => {
+      // 向上滾動回來時移除 class
+      header.classList.remove('header--light');
+      header.classList.add('header--dark');
+    },
   });
 }
