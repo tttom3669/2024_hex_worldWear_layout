945fc586db99f65f52c501434b32cb80b6040f19
Abicat <79410104+Abicat@users.noreply.github.com>
2024-11-02 00:59:30 +0800

    調整卡片折扣功能及樣式、調整類別篩選選單手機版取消按鈕樣式

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,101 +1,121 @@
 export function productRender(productListCard, productList) {
 
-  let str = '';
+  let str = "";
 
-  productList.forEach(item => {
-      
-      const { statusCard, statusClassNames, shouldHide } = getStatusClass(item.status);
-      const { discount } = getDiscountPrice(item.discountPrice);
+  productList.forEach((item) => {
 
-      str += `
-        <div class="col-6 col-md-4">
+    const { statusCard, statusClassNames, shouldHide } = getStatusClass(
+      item.status
+    );
+
+    const discountPrice =
+      item.discount > 0 ? Math.floor(item.price - item.discount) : null;
+
+    str += `
+      <div class="col-6 col-md-4">
           <a href="#" class="productList-card card-column h-100">
               <div class="card g-3 h-100">
                   <div class="w-100 h-75 overflow-hidden img-wrapper position-relative rounded-top ${statusCard} productList-card-img">
-                    <img src="/assets/images/product/${item.img}" 
-                          class="card-img-top cover-img w-100 h-md-100 ${shouldHide ? 'mask' : ''}" 
-                          alt="productList-image"
-                          style="object-fit: cover;">
-                    <div class="position-absolute bottom-0 end-0 p-2 favorite-div">
-                      <span >
-                        <button type="button" class="btn btn-outline-danger favorite-button rounded-circle align-middle" data-favorite="false">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart heartIcon-button" viewBox="0 0 16 16">
-                            <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"
-                             stroke="transparent"/>
-                          </svg>
-                        </button>
-                      </span>
-                    </div>
+                      <img src="/assets/images/product/${item.img}" 
+                           class="card-img-top cover-img w-100 h-md-100 ${
+                             shouldHide ? "mask" : ""
+                           }" 
+                           alt="productList-image" style="object-fit: cover;">
+                      <div class="position-absolute bottom-0 end-0 p-2 favorite-div">
+                          <span>
+                              <button type="button" class="btn btn-outline-danger favorite-button rounded-circle align-middle" data-favorite="false">
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart heartIcon-button" viewBox="0 0 16 16">
+                                      <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" stroke="transparent"/>
+                                  </svg>
+                              </button>
+                          </span>
+                      </div>
                   </div>
-                  <div class="card-img-overlay text-white ${shouldHide ? '' : 'd-none'}">
-                    <h3 class="card-title fst-italic font-dm-serif">Soldout</h5>
-                    <p class="card-text"><h5>已售完</h5></p>
+                  <div class="card-img-overlay text-white ${
+                    shouldHide ? "" : "d-none"
+                  }">
+                      <h3 class="card-title fst-italic font-dm-serif">Soldout</h5>
+                      <p class="card-text"><h5>已售完</h5></p>
                   </div>
-                  <div class="${statusClassNames} position-absolute opacity-70 ">
+                  <div class="${statusClassNames} position-absolute opacity-70">
                       <span class="me-1">${item.status}</span>
                   </div>  
-                  <div class="card-body p-0 mt-3 mt-lg-4 d-flex justify-content-between flex-column">
+                  <div class="card-body p-0 mt-2 d-flex justify-content-between flex-column bg-nature-99">
                       <div class="d-flex justify-content-between align-items-center">
-                        <h5 class="card-title fs-sm fs-lg-6 ms-2 mb-2">${item.productName}</h5>
-                        <p class="d-flex justify-content-center align-items-center me-2">
-                          <span class="text-center">
-                            <button type="button" class="btn btn-outline-danger favorite rounded-circle align-middle" data-favorite="false">
-                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart heartIcon" viewBox="0 0 16 16">
-                              <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"
-                               stroke="transparent"/>
-                             </svg>
-                            </button>
-                          </span>
-                        </p>
+                          <h5 class="card-title fs-sm fs-lg-6 ms-lg-2">${
+                            item.productName
+                          }</h5>
+                          <p class="d-flex justify-content-center align-items-center me-2">
+                              <span class="text-center">
+                                  <button type="button" class="btn btn-outline-danger favorite rounded-circle align-middle" data-favorite="false">
+                                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart heartIcon" viewBox="0 0 16 16">
+                                          <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" stroke="transparent"/>
+                                      </svg>
+                                  </button>
+                              </span>
+                          </p>
                       </div>
-                      <div class="d-flex justify-content-between align-items-center mb-7">
-                        <div class="d-flex align-items-center mt-1 mt-lg-2">
-                          <div class="d-flex justify-content-center align-items-center ms-2">
-                            <span class="${discount ? 'd-none' : ''} discountText fs-sm fs-lg-6">
-                              ${item.discountPrice}
-                            </span>
-                            <p class="fs-sm fs-lg-6 ${discount ? '' : 'cost'}">
-                              ${item.price}
-                            </p>
+                      <div class="d-flex justify-content-between align-items-center mb-7 ms-lg-2">
+                          <div class="d-flex align-items-center mt-1">
+                              <div class="d-flex justify-content-center align-items-center">
+                                  ${
+                                    item.discount > 0
+                                      ? `
+                                      <span class="fs-sm fs-lg-6 me-1 listPrice">$${formatNumber(
+                                        item.price
+                                      )}</span>
+                                      <p class="fs-sm fs-lg-6 discountPrice">$${formatNumber(
+                                        discountPrice
+                                      )}</p>
+                                  `
+                                      : `
+                                      <p class="fs-sm fs-lg-6">$${formatNumber(
+                                        item.price
+                                      )}</p>
+                                  `
+                                  }
+                              </div>
                           </div>
-                        </div>
                       </div>
                   </div>
               </div>
           </a>                            
-        </div>
       </div>
-      `
-    });
-    productListCard.innerHTML = str;
+      `;
+  });
 
-    function getDiscountPrice(discount) {
-      return discount > 0;
+  productListCard.innerHTML = str;
+
+  function getStatusClass(status) {
+
+    let statusCard = "";
+    let statusClassNames = "";
+    let shouldHide = false;
+
+    switch (status) {
+      case "補貨中":
+        statusClassNames = "productList-category-tag status-new";
+        break;
+
+      case "預購":
+        statusClassNames = "productList-category-tag status-hot";
+        break;
+
+      case "現貨":
+        statusClassNames = "productList-category-tag status-default";
+        break;
+
+      default:
+        statusClassNames = "productList-category-soldOut-tag";
+        statusCard = "bg-dark";
+        shouldHide = true;
+        break;
     }
-      
-    function getStatusClass(status) {
 
-      let statusCard       = '';
-      let statusClassNames = '';
-      let shouldHide       = false;
-
-      switch (status) {
-          case '補貨中':
-              statusClassNames = 'productList-category-tag status-new';           
-              break;
-          case '預購':
-              statusClassNames = 'productList-category-tag status-hot';        
-              break;
-          case '現貨':
-              statusClassNames = 'productList-category-tag status-default';           
-              break;
-          default:
-              statusClassNames = 'productList-category-soldOut-tag';
-              statusCard  = 'bg-dark';
-              shouldHide = true;
-              break;
-      } 
-      return { statusCard, statusClassNames, shouldHide};
-    }
+    return { statusCard, statusClassNames, shouldHide };
   }
- 
+
+  function formatNumber(num) {
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+}
--- assets/js/productList.js
+++ assets/js/productList.js
@@ -1,106 +1,78 @@
-import { productRender } from './product.js';
+import { productRender } from "./product.js";
 
-const productListCard = document.querySelector('.productList-card-wrap');
+const productListCard = document.querySelector(".productList-card-wrap");
 const productListCardList = [
-    {
-        productName: '休閒寬鬆褲',
-        img: 'product_001.png',
-        status: '預購',
-        price: 'NTD.800',
-        discountPrice:'NTD.950'
-      },
-      {
-        productName: '印度有機棉T恤',
-        img: 'product_002.png',
-        status: '預購',
-        price: 'NTD.1,290',
-        discountPrice:''
-      },
-      {
-        productName: '優雅襯衫',
-        img: 'product_003.png',
-        status: '現貨',
-        price: 'NTD.590',
-        discountPrice:''
-      },
-      {
-        productName: '柔軟針織衫',
-        img: 'product_004.png',
-        status: '現貨',
-        price: 'NTD.1,290',
-        discountPrice:''
-      },
-      {
-        productName: '牛仔長裙',
-        img: 'product_005.png',
-        status: '現貨',
-        price: 'NTD.2,290',
-        discountPrice:''
-      },
-      {
-        productName: '商務彈性長袖襯衫',
-        img: 'product_006.png',
-        status: '現貨',
-        price: 'NTD.490',
-        discountPrice:''
-      },
-      {
-        productName: '條紋襯衫裙',
-        img: 'product_007.png',
-        status: '補貨中',
-        price: 'NTD.890',
-        discountPrice:''
-      },
-      {
-        productName: '法式薄紗襯衫',
-        img: 'product_008.png',
-        status: '補貨中',
-        price: 'NTD.1,290',
-        discountPrice:''
-      },
-      {
-        productName: '素雅亞麻長褲',
-        img: 'product_009.png',
-        status: '',
-        price: 'NTD.690',
-        discountPrice:''
-      }
-      // ,{
-      //   productName: '素雅亞麻長褲',
-      //   img: 'product_009.png',
-      //   status: '',
-      //   price: 'NTD.690',
-      //   discountPrice:''
-      // },
-      // {
-      //   productName: '素雅亞麻長褲',
-      //   img: 'product_009.png',
-      //   status: '',
-      //   price: 'NTD.690',
-      //   discountPrice:''
-      // },
-      // {
-      //   productName: '素雅亞麻長褲',
-      //   img: 'product_009.png',
-      //   status: '',
-      //   price: 'NTD.690',
-      //   discountPrice:''
-      // },
-      // {
-      //   productName: '素雅亞麻長褲',
-      //   img: 'product_009.png',
-      //   status: '',
-      //   price: 'NTD.690',
-      //   discountPrice:''
-      // }
+  {
+    productName: "休閒寬鬆褲",
+    img: "product_001.png",
+    status: "預購",
+    price: 900,
+    discount: 10,
+  },
+  {
+    productName: "印度有機棉T恤",
+    img: "product_002.png",
+    status: "預購",
+    price: 1290,
+    discount: 40,
+  },
+  {
+    productName: "優雅襯衫",
+    img: "product_003.png",
+    status: "現貨",
+    price: 590,
+    discount: 0,
+  },
+  {
+    productName: "柔軟針織衫",
+    img: "product_004.png",
+    status: "現貨",
+    price: 1290,
+    discount: 0,
+  },
+  {
+    productName: "牛仔長裙",
+    img: "product_005.png",
+    status: "現貨",
+    price: 2290,
+    discount: 0,
+  },
+  {
+    productName: "商務彈性長袖襯衫",
+    img: "product_006.png",
+    status: "現貨",
+    price: 490,
+    discount: 0,
+  },
+  {
+    productName: "條紋襯衫裙",
+    img: "product_007.png",
+    status: "補貨中",
+    price: 890,
+    discount: 0,
+  },
+  {
+    productName: "法式薄紗襯衫",
+    img: "product_008.png",
+    status: "補貨中",
+    price: 1290,
+    discount: 0,
+  },
+  {
+    productName: "素雅亞麻長褲",
+    img: "product_009.png",
+    status: "",
+    price: 690,
+    discount: 0,
+  },
 ];
 
 if (productListCard) {
-    const productListRenderData = () => {
-        productRender(productListCard, productListCardList);
-    };
-  
-    productListRenderData();
-  } else {
-    console.error('roductList-card-wrap element not found');
-  }
+  const productListRenderData = () => {
+    productRender(productListCard, productListCardList);
+  };
+
+  productListRenderData();
+} else {
+  console.error("roductList-card-wrap element not found");
+}
